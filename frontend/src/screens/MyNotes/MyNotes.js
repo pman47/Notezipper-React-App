045f149b4ddeca84c88cc28eb06db37f144be547
Manish <manishprajapatim416@gmail.com>
2021-12-13 16:00:21 +0530

    MongoDB Connected

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,9 +1,27 @@
-import { Button } from "react-bootstrap";
-import React from "react";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
+import axios from "axios";
 
 const MyNotes = () => {
+  const [notes, setNotes] = useState([]);
+
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+    }
+  };
+
+  const fetchNotes = async () => {
+    const { data } = await axios.get("/api/notes");
+    setNotes(data);
+  };
+  console.log(notes);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <MainScreen title="Welcome back Manish Prajapati...">
       <Link to="createnote">
@@ -11,6 +29,53 @@ const MyNotes = () => {
           Create new Note
         </Button>
       </Link>
+      {notes.map((note) => (
+        <Accordion key={note._id}>
+          <Accordion.Item eventKey="0">
+            <Card style={{ margin: 10 }}>
+              <Card.Header style={{ display: "flex" }}>
+                <span
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    cursor: "pointer",
+                    alignSelf: "center",
+                    fontSize: 18,
+                  }}
+                >
+                  <Accordion.Header>{note.title}</Accordion.Header>
+                </span>
+                <div>
+                  <Button href={`/note/${note._id}`}>Edit</Button>
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
+              <Accordion.Body>
+                <Card.Body>
+                  <h4>
+                    <Badge className="bg-success" style={{ color: "white" }}>
+                      Category - {note.category}
+                    </Badge>
+                  </h4>
+                  <blockquote className="blockquote mb-0">
+                    <p>{note.content}</p>
+                    <footer className="blockquote-footer">
+                      Created On - date
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Body>
+            </Card>
+          </Accordion.Item>
+        </Accordion>
+      ))}
     </MainScreen>
   );
 };
